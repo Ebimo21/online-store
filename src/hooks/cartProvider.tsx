65205e09d9a8345eb09f320c544d7db3e36dcacb
ebimo21 <ebi4jah15@gmail.com>
2023-06-01 23:34:11 +0100

    removed cookies

--- src/hooks/cartProvider.tsx
+++ src/hooks/cartProvider.tsx
@@ -2,7 +2,7 @@
 import { categories } from "@/database/db";
 import { SetStateAction, useEffect, useMemo } from "react";
 import { createContext, Dispatch, useContext, useState } from "react";
-import { getCookie, setCookie } from "cookies-next";
+// import { getCookie, setCookie } from "cookies-next";
 
 type CartContextType = {
     cart: ShopItem[]; 
@@ -30,42 +30,42 @@ export default function useCart(){
 
 export const CartProvider = ({children}: Props) =>{
 
-    if(!getCookie('store')){
-        setCookie('store', JSON.stringify([]))
-    }
+    // if(!getCookie('store')){
+    //     setCookie('store', JSON.stringify([]))
+    // }
 
-    if(!getCookie('storeCount')){
-        setCookie('storeCount', JSON.stringify(0))
-    }
+    // if(!getCookie('storeCount')){
+    //     setCookie('storeCount', JSON.stringify(0))
+    // }
     
     const Storestr = JSON.stringify([{}])
     const Countstr = JSON.stringify(0)
 
-    const tempCookieStore:string =  String(getCookie('store')) || Storestr;
-    const CookieStoreCount:number = Number(getCookie('storeCount')) ||  Number(Countstr);
-    const CookieStore:ShopItem[]  = JSON.parse(tempCookieStore);
+    // const tempCookieStore:string =  String(getCookie('store')) || Storestr;
+    // const CookieStoreCount:number = Number(getCookie('storeCount')) ||  Number(Countstr);
+    // const CookieStore:ShopItem[]  = JSON.parse(tempCookieStore);
     
-    const [cart, setCart] = useState<Array<ShopItem>>(CookieStore);
-    const [cartCount, setCartCount] = useState<number>(CookieStoreCount);
-    const [inventory, setInventory] = useState<Array<ShopItem>>(categories);
+    const [cart, setCart] = useState<ShopItem[]>(categories);
+    const [cartCount, setCartCount] = useState<number>(0);
+    const [inventory, setInventory] = useState<ShopItem[]>(categories);
 
     const cartTotal = useMemo(()=>{
         let total = 0;
-        cart?.map((item, index)=> total += (item.amount * item.count));
+        cart.map((item, index)=> total += (item.amount * item.count));
         return total
     }, [cart])
 
     const removeFromCart = (item:ShopItem):void =>{
-        const tempCookieStore:string =  String(getCookie('store'));
-        const CookieStoreCount = Number(getCookie('storeCount'));
+        // const tempCookieStore:string =  String(getCookie('store'));
+        // const CookieStoreCount = Number(getCookie('storeCount'));
         
-        const CookieStore:ShopItem[]  = JSON.parse(tempCookieStore);
-        const found = CookieStore.find((cartItem)=> cartItem.id == item.id);
+        // const CookieStore:ShopItem[]  = JSON.parse(tempCookieStore);
+        const found = cart.find((cartItem)=> cartItem.id == item.id);
 
         if(found){
-            const modifiedCookieStore:ShopItem[] = CookieStore.filter(item => item.id !== found.id);
-            setCookie('store', modifiedCookieStore);
-            setCookie('storeCount', CookieStoreCount - 1);
+            const modifiedCookieStore:ShopItem[] = cart.filter(item => item.id !== found.id);
+            // setCookie('store', JSON.stringify(modifiedCookieStore));
+            // setCookie('storeCount', String(CookieStoreCount - 1));
             console.log(modifiedCookieStore);
             setCart(modifiedCookieStore);
             setCartCount(prev=> prev-1)
@@ -73,26 +73,26 @@ export const CartProvider = ({children}: Props) =>{
     }
 
     const addToCart = (item:ShopItem):void =>{
-        const temp =  String(getCookie('store'));
-        const CookieStore:ShopItem[]  = JSON.parse(temp);
-        const found = CookieStore.find((cartItem)=> cartItem.id == item.id )
+        // const temp =  String(getCookie('store'));
+        // const CookieStore:ShopItem[]  = JSON.parse(temp);
+        const found = cart.find((cartItem)=> cartItem.id == item.id )
         
         if(!found){
-            const newItem:ShopItem[] = [...CookieStore, {...item, count: 1}]
-            setCookie('store', JSON.stringify(newItem));
-            setCookie('storeCount', cartCount + 1)
+            const newItem:ShopItem[] = [...cart, {...item, count: 1}]
+            // setCookie('store', JSON.stringify(newItem));
+            // setCookie('storeCount', cartCount + 1)
             setCart(newItem);
             setCartCount(prev=>prev+1)
         } else {
             
-            const modifiedCookieStore = CookieStore.map((prevItem)=>{
+            const modifiedCookieStore = cart.map((prevItem)=>{
                 if(prevItem.id === item.id){
                     return {...prevItem, count: prevItem.count + 1}
                 }
                 return prevItem;
             });
-
-            setCookie('store', modifiedCookieStore);
+// 
+            // setCookie('store', modifiedCookieStore);
             setCart(modifiedCookieStore);
         }
     }
